426ad47b0792244abdf0e94ec2d45db4d087c9cc
CHHORN Seyha <senjihachiro@gmail.com>
2025-09-05 16:05:07 +0700

    test gateway

---
+++ gateway-service/src/main/java/org/homework/GatewayServiceApplication.java
@@ -0,0 +1,37 @@
+package org.homework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class GatewayServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayServiceApplication.class, args);
+    }
+
+//    @Bean
+//    public RouteLocator myRoute(RouteLocatorBuilder builder){
+//        return builder.routes()
+//                .route("user_service",r-> r.path("/users/**")
+//                        .uri("http://localhost:8081"))
+//                .route("product_service", r-> r.path("/products/**")
+//                        .uri("http://localhost:8082"))
+//                .build();
+//    }
+
+    @Bean
+    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(p -> p
+                        .path("/get")
+                        .filters(f -> f.addRequestHeader("Hello", "World"))
+                        .uri("http://httpbin.org:80"))
+                .build();
+    }
+
+}
---
+++ product-service/src/main/java/org/homework/ProductServiceApplication.java
@@ -0,0 +1,20 @@
+package org.homework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ProductServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ProductServiceApplication.class, args);
+    }
+
+    @GetMapping("/products/{id}")
+    public String getProduct(@PathVariable String id) {
+        return "Product " + id + " from product-service!";
+    }
+}
